2c4083462874b1e426120cbbfbc6ed8bf28ebe18
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-01-14 13:56:31 -0500

    add project and 2 homeworks, styled the pages, removed Contact page

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
+
+  function sendMessage() {
+    timeEl.textContent = " ";
+  
+    var imgEl = document.createElement("img");
+  
+    imgEl.setAttribute("src", "images/image_1.jpg");
+    mainEl.appendChild(imgEl);
+  
+  }
+  
+  setTime();
